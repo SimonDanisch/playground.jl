55cc09566c90d93f6d11b8172e2e2dd95834d0fb
SimonDanisch <sdanisch@gmail.com>
2017-01-24 02:50:14 +0100

    updates

--- isabel/eigshow_img2.jl
+++ isabel/eigshow_img2.jl
@@ -1,29 +1,61 @@
 using GLVisualize, Colors, Reactive, GLWindow, GeometryTypes
 using Images, StaticArrays, GLAbstraction, Iterators
 import GeometryTypes: intersects
-w = glscreen(); @async GLWindow.waiting_renderloop(w)
+import GLVisualize: mm
+window = glscreen(); @async GLWindow.waiting_renderloop(window)
 animate(t, A) = t*eye(A) + (1-t)*A
-animate(t, A) = Scalar(t*eye(A) + (1-t)*A)
-img = loadasset("racoon.png")
+
+iconsize = 8mm
+editarea, viewarea = x_partition_abs(window.area, round(Int, 8.2 * iconsize))
+edit_screen = Screen(
+    window, area = editarea,
+    stroke = (1f0, RGBA{Float32}(0.9f0, 0.9f0, 0.9f0, 1f0))
+)
+viewscreen = Screen(
+    window, area = viewarea,
+)
+
+img = load("/home/s/Desktop/kitty.jpg")
 A = Mat{2}(1, 3, 4, 2) ./ 4
-sv, t = GLVisualize.slider(0:0.01:1.0, w)
-_view(sv, camera = :fixed_pixel)
+sv, t = GLVisualize.labeled_slider(linspace(1.0,0.0,100), edit_screen)
+matrix = map(t-> animate(t, A), t)
+matrix_str = map(matrix) do m
+    str = ""
+    for i=1:2
+        for j=1:2
+            str *= string(@sprintf("%15.4f", m[i, j]), " ")
+        end
+        str *= "\n"
+    end
+    str
+end
+
+det_str = map(matrix) do m
+    @sprintf("%15.4f", det(m))
+end
+
+menu = Pair[
+    "slider:" => sv,
+    "matrix:" => matrix_str,
+    "determinant:" => det_str
+]
+_view(visualize(
+    menu,
+    text_scale = 4mm,
+    width = 8iconsize
+), edit_screen, camera = :fixed_pixel)
+
 prim_rect = SimpleRectangle(-250, -250, 500, 500)
 mesh = GLUVMesh(prim_rect)
 prim = map(t) do t
     points = decompose(Point2f0, prim_rect)
-    points .= (*).(animate(1-t, A), points)
+    points .= (*).(Scalar(animate(t, A)), points)
     mesh.vertices[:] = map(x-> Point3f0(x[1], x[2], 0), points)
     mesh
 end
-_view(visualize(img, fxaa = true, primitive = prim, boundingbox = nothing))
+_view(visualize(img, fxaa = true, primitive = prim, boundingbox = nothing), viewscreen)
 
-a = eigfact(Array(A))
-eigvectors = map(1:size(a.vectors, 1)) do i
-    normalize(Vec2f0(a.vectors[i, :]...))
-end
-v1 = eigvectors[1] * 1000f0
-v2 = eigvectors[2] * 1000f0
+
 origin = Point2f0(0)
 lines = Point2f0[]
 
@@ -39,12 +71,21 @@ function sort_rectangle!(points)
     p1 = first(points)
     sort!(points, by = p-> vec_angle(middle, p, p1))
 end
-eigvectpoly = map(prim) do prim
+
+eigvectpoly = map(t) do t
     # bring vertices in correct order and close rectangle
-    points = sort_rectangle!(map(Point2f0, vertices(prim)))
+    points = sort_rectangle!(map(Point2f0, vertices(value(prim))))
     push!(points, points[1]) # close points
-    eigseg1 = LineSegment(origin, Point2f0(v1))
-    eigseg2 = LineSegment(origin, Point2f0(v2))
+
+    a = eigfact(Array(A))
+    eigvectors = map(1:size(a.vectors, 1)) do i
+        normalize(Vec2f0(a.vectors[:, i]...))
+    end
+    v1 = eigvectors[1] * 1000f0
+    v2 = eigvectors[2] * 1000f0
+    m = animate(t, A)
+    eigseg1 = LineSegment(origin, Point2f0(m*v1))
+    eigseg2 = LineSegment(origin, Point2f0(m*v2))
     seg1cut = seg2cut = (0, origin)
     for (i, (a, b)) in enumerate(partition(points, 2, 1))
         seg = LineSegment(a,b)
@@ -54,30 +95,22 @@ eigvectpoly = map(prim) do prim
         intersected && (seg2cut = (i, p))
     end
     pop!(points) #remove closing point
-    cutout = Point2f0[seg2cut[2], origin, seg1cut[2]]
-    i1, i2 = seg1cut[1], seg2cut[1]
-    imin, imax = min(i1, i2), max(i1, i2)
-    i = if imax - imin <= 1
-        (imin:(imax-imin)) + 1
-    else
-        1:(imin + 4 - imax)
-    end
-    splice!(points, i, cutout)
     GLPlainMesh(points), Point2f0[seg2cut[2], origin, seg1cut[2]]
 end
-_view(visualize(
-    map(first, eigvectpoly),
-    color = RGBA(1f0, 1f0, 1f0, 0.6f0),
-), camera = :orthographic_pixel)
+# _view(visualize(
+#     map(first, eigvectpoly),
+#     color = RGBA(1f0, 1f0, 1f0, 0.6f0),
+# ), camera = :orthographic_pixel)
 
 _view(visualize(
     map(last, eigvectpoly), :linesegment,
     indices = [2, 1, 2, 3],
     thickness = 3f0,
     color = RGBA(0.60, 0.3f0, 0.4f0, 1f0),
-), camera = :orthographic_pixel)
+), viewscreen, camera = :orthographic_pixel)
 
 _view(visualize(
     (Circle(Point2f0(0), 5f0), map(x-> map(Point2f0, vertices(x)), prim)),
     color = RGBA(0.7f0, 0.2f0, 0.9f0, 1f0),
-), camera = :orthographic_pixel)
+), viewscreen, camera = :orthographic_pixel)
+center!(viewscreen, :orthographic_pixel, border = 10f0)
--- signals/imageproc.jl
+++ signals/imageproc.jl
@@ -14,16 +14,15 @@ view_node(gn3, screen, (20, 20, area...), add_menu = false)
 gn4 = graphnode(Signal(map(RGB{U8}, loadasset("racoon.png"))), Signal(1.0f0)) do img, sigma
     imfilter_gaussian(img, [sigma, sigma])
 end
+
 s = view_node(gn4, screen, (100, 50, 100, 100))
 
-gn5 = graphnode(Signal(map(RGB{U8}, loadasset("racoon.png")))) do img
-    map(RGB{U8}, restrict(img))
-end
-s = view_node(gn5, screen, (500, 50, 100, 100))
-
-gn6 = graphnode(Signal(map(RGB{Float64}, loadasset("racoon.png"))); style = :surface) do img
-    map(img) do color
-        Float32(Gray(color))
-    end.data
+@inline grayf32(color) = Float32(Gray(color))::Float32
+bb = AABB(Vec3f0(0), Vec3f0(1))
+gn6 = graphnode(
+        Signal(map(RGB{Float64}, loadasset("racoon.png")));
+        boundingbox = bb, style = :surface
+    ) do img
+    map(grayf32, img).data
 end
 s = view_node(gn6, screen, (500, 50, 100, 100))
